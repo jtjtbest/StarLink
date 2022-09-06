d55eb865415b86c5f32c2d8635b8a56804607798
jtjtbest <jintao5126@hotmail.com>
2022-09-06 12:02:59 -0400

    Add existing project files prior to the push to GitHub.

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const SAT_API_KEY = "TN6TH6-ZJJ852-K42T43-4WFL";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://18.118.107.1";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
+
